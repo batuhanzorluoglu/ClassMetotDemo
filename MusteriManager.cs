ccdadb0eb5353661dee4f4d2ba662c9c6a2a1393
batuhan <batuhan@DESKTOP-9F08VRM>
2021-01-26 22:38:46 +0300

    first commit

---
+++ MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi :" + musteri.musteriAdi + " " + musteri.musteriSoyadi + " " + musteri.musteriDogumYeri + " " + musteri.Id);
+        }
+        
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi :" + musteri.musteriAdi + " " + musteri.musteriSoyadi + " " + musteri.musteriDogumYeri + " " + musteri.Id);
+        }
+            
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : " + musteri.musteriAdi + " " + musteri.musteriSoyadi + " " + musteri.musteriDogumYeri + " " + musteri.Id);
+        }
+        
+            
+            
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.musteriAdi = "Batuhan";
+            musteri1.musteriSoyadi = "Zorluoğlu";
+            musteri1.musteriDogumYeri = "İskenderun";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.musteriAdi = "Cihat";
+            musteri2.musteriSoyadi = "İleri";
+            musteri2.musteriDogumYeri = "Adana";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Adı :"+musteri.musteriAdi+" "+"Müşteri Soyadı :"+musteri.musteriSoyadi+" "+"Müşteri Id :"+musteri.Id+" "+"Müşteri Doğum Yeri : "+musteri.musteriDogumYeri);
+            }
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            MusteriManager musteriListeleme = new MusteriManager();
+            musteriListeleme.Listele(musteri1);
+            musteriListeleme.Listele(musteri2);
+
+            MusteriManager musteriSilme = new MusteriManager();
+            musteriSilme.Silme(musteri1);
+            musteriSilme.Silme(musteri2);
+            
+
+        }
+    }
+}
